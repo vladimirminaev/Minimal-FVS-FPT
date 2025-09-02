e8e0d3745fe37cbea8fca3c75fad2a69d4a78bd7
vladimirminaev <vladimir.minaev2002@gmail.com>
2025-09-02 21:14:01 +0200

    Deleting outdates files

--- code/utils.py
+++
@@ -1,5 +0,0 @@
-from itertools import chain, combinations
-
-def powerset(iterable):
-    s = list(iterable)
-    return [list(item) for item in chain.from_iterable(combinations(s, r) for r in range(len(s)+1))]
